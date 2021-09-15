9ef3ac6a5426a7e42108ea8cfb49173e54e2a3eb
lenguyetchausa19486991 <90306087+lenguyetchausa19486991@users.noreply.github.com>
2021-09-15 22:38:54 +0800

    Add files via upload

---
+++ Bai2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        int temp;
+        boolean p = true;
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Nhap so cần kiem tra");
+        int num = sc.nextInt();
+        sc.close();
+        for (int i = 2; i <= num / 2; i++) {
+            temp = num % i;
+            if (temp == 0) {
+                p = false;
+                break;
+
+            }
+        }
+        if (p)
+            System.out.println(num + "la so nguyen to");
+        else
+            System.out.println(num + "khong la so nguyen to");
+    }
+
+}
